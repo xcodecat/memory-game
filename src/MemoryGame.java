e4f8bf35b053b82d6c3d21ae3be8d3aee115b126
xcodecat <xmissh0lyc4t@gmail.com>
2023-06-19 18:58:35 +0200

    comments are now in german

--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -6,81 +6,69 @@ import java.util.List;
 
 public class MemoryGame extends JFrame {
 
-    private final List<MemoryButton> buttons; // The list of memory buttons
-    private final ImageIcon coverIcon; // The icon for the covered card
-    private final List<ImageIcon> imageIcons; // The list of image icons for the game
+    private final List<MemoryButton> buttons;
+    private final ImageIcon coverIcon;
+    private final List<ImageIcon> imageIcons;
 
-    private MemoryButton selectedButton; // The currently selected button
-    private int matchedPairs; // The number of matched pairs
+    private MemoryButton selectedButton;
+    private int matchedPairs;
 
     public MemoryGame() {
-        setTitle ("Memory Game");
-        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        setResizable (true);
+        setTitle("Memory Spiel");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
 
-        // The panel that contains the memory buttons
-        JPanel panel = new JPanel (new GridLayout (4, 4, 5, 5)); // The panel is set up as a 4x4 grid
-        buttons = new ArrayList<> (); // Initializing the list for memory buttons
-        coverIcon = new ImageIcon ("src/images/cover.png"); // The icon for the covered card
-        imageIcons = loadImages (); // Loading the image icons for the game
+        JPanel panel = new JPanel (new GridLayout (4, 4, 5, 5));
+        buttons = new ArrayList<>();
+        coverIcon = new ImageIcon("src/images/cover.png");
+        imageIcons = loadImages();
 
-        // Create two copies of each image icon
-        List<ImageIcon> pairedIcons = new ArrayList<> ();
+        // Erstelle zwei Kopien von jedem Bildsymbol
+        List<ImageIcon> pairedIcons = new ArrayList<>();
         for (ImageIcon icon : imageIcons) {
-            pairedIcons.add (icon);
-            pairedIcons.add (icon);
+            pairedIcons.add(icon);
+            pairedIcons.add(icon);
         }
 
-        // Shuffle the paired icons
-        Collections.shuffle (pairedIcons);
+        // Mische die Bildsymbole
+        Collections.shuffle(pairedIcons);
 
-        // Create memory buttons for each icon and add them to the panel
         for (ImageIcon icon : pairedIcons) {
-            MemoryButton button = new MemoryButton (icon);
-            button.addActionListener (event -> buttonClicked (button));
-            buttons.add (button);
-            panel.add (button);
+            MemoryButton button = new MemoryButton(icon);
+            button.addActionListener(event -> buttonClicked(button));
+            buttons.add(button);
+            panel.add(button);
         }
 
-        add (panel);
-        pack ();
-        setLocationRelativeTo (null);
-        setVisible (true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater (MemoryGame::new);
+        add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     private void buttonClicked(MemoryButton button) {
-        button.showImage ();
+        button.showImage();
 
         if (selectedButton == null) {
-            // Keine andere Karte ausgewählt
+            // Keine Karte ist ausgewählt
             selectedButton = button;
         } else {
             // Eine andere Karte ist bereits ausgewählt
-            if (selectedButton.getImageIcon () == button.getImageIcon ()) {
-                // Die beiden Karten passen zusammen
-                selectedButton.setMatched (true);
-                button.setMatched (true);
-                selectedButton.setEnabled (false);
-                button.setEnabled (false);
+            if (selectedButton.getImageIcon() == button.getImageIcon()) {
+                // Paar gefunden
+                selectedButton.setMatched(true);
+                button.setMatched(true);
+                selectedButton.setEnabled(false);
+                button.setEnabled(false);
                 selectedButton = null;
 
-                hideMatchedPairs (); // Verstecke gefundene Paare
+                hideMatchedPairs(); // Verstecke gefundene Paare
 
                 // Überprüfe, ob alle Paare gefunden wurden
-                int numMatchedPairs = 0;
-                for (MemoryButton btn : buttons) {
-                    if (btn.isMatched ()) {
-                        numMatchedPairs++;
-                    }
-                }
-                if (numMatchedPairs == buttons.size ()) {
-                    // Alle Paare wurden gefunden
+                if (matchedPairs == imageIcons.size()) {
+                    // Spiel beendet
                     Object[] options = {"Neue Runde", "Beenden"};
-                    int choice = JOptionPane.showOptionDialog (this,
+                    int choice = JOptionPane.showOptionDialog(this,
                             "Herzlichen Glückwunsch! Du hast das Spiel gewonnen!",
                             "Spiel beendet",
                             JOptionPane.DEFAULT_OPTION,
@@ -90,29 +78,29 @@ public class MemoryGame extends JFrame {
                             options[0]);
 
                     if (choice == 0) {
-                        resetGame ();
+                        resetGame();
                     } else {
-                        System.exit (0);
+                        System.exit(0);
                     }
                 }
             } else {
-                // Die beiden Karten passen nicht zusammen
-                Timer timer = new Timer (1000, e -> {
-                    selectedButton.hideImage ();
-                    button.hideImage ();
+                // Kein Paar gefunden
+                Timer timer = new Timer(1000, e -> {
+                    selectedButton.hideImage();
+                    button.hideImage();
                     selectedButton = null;
                 });
-                timer.setRepeats (false);
-                timer.start ();
+                timer.setRepeats(false);
+                timer.start();
             }
         }
     }
 
     private void hideMatchedPairs() {
-        matchedPairs += 2; // Increment matchedPairs by 2
         for (MemoryButton button : buttons) {
-            if (button.isMatched ()) {
-                button.setEnabled (false);
+            if (button.isMatched()) {
+                button.setBackground(Color.GRAY);
+                button.setEnabled(false);
             }
         }
     }
@@ -121,29 +109,33 @@ public class MemoryGame extends JFrame {
         selectedButton = null;
         matchedPairs = 0;
 
-        // Reset the game by hiding all buttons and reshuffling the icons
+        // Setze das Spiel zurück, indem alle Karten versteckt werden und die Bildsymbole neu gemischt werden
         for (MemoryButton button : buttons) {
-            button.hideImage ();
-            button.setMatched (false);
-            button.setEnabled (true);
-            button.setBackground (null); // Reset background color
+            button.hideImage();
+            button.setMatched(false);
+            button.setEnabled(true);
+            button.setBackground(null); // Setze Hintergrundfarbe zurück
         }
-        Collections.shuffle (imageIcons);
+        Collections.shuffle(imageIcons);
         int i = 0;
         for (MemoryButton button : buttons) {
-            button.setImageIcon (imageIcons.get (i));
+            button.setImageIcon(imageIcons.get(i));
             i++;
         }
     }
 
     private List<ImageIcon> loadImages() {
-        List<ImageIcon> icons = new ArrayList<> ();
+        List<ImageIcon> icons = new ArrayList<>();
         for (int i = 0; i < 15; i++) {
             String filename = "src/images/" + i + ".png";
-            ImageIcon icon = new ImageIcon (filename);
-            icons.add (icon);
+            ImageIcon icon = new ImageIcon(filename);
+            icons.add(icon);
         }
         return icons;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MemoryGame::new);
     }
 
     class MemoryButton extends JButton {
@@ -151,24 +143,24 @@ public class MemoryGame extends JFrame {
         private boolean matched;
 
         public MemoryButton(ImageIcon icon) {
-            setIcon (coverIcon);
-            setPreferredSize (new Dimension (100, 100));
-            setMargin (new Insets (0, 0, 0, 0));
-            setContentAreaFilled (false);
-            setBorderPainted (false);
-            setOpaque (true);
-            setBackground (Color.WHITE);
+            setIcon(coverIcon);
+            setPreferredSize(new Dimension(100, 100));
+            setMargin(new Insets(0, 0, 0, 0));
+            setContentAreaFilled(false);
+            setBorderPainted(false);
+            setOpaque(true);
+            setBackground(Color.WHITE);
 
             imageIcon = icon;
             matched = false;
         }
 
         public void showImage() {
-            setIcon (imageIcon);
+            setIcon(imageIcon);
         }
 
         public void hideImage() {
-            setIcon (coverIcon);
+            setIcon(coverIcon);
         }
 
         public ImageIcon getImageIcon() {
