9995fd237512703d65acb4ecaed3b913e34a311a
xcodecat <xmissh0lyc4t@gmail.com>
2023-07-06 12:31:07 +0200

    fix: fixed some errors. - 2 turns for first player when startup - game finishing after half of the pairs where fount

--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -9,9 +9,11 @@ public class MemoryGame extends JFrame {
     private final List<MemoryButton> buttons;
     private final ImageIcon coverIcon;
     private final List<ImageIcon> imageIcons;
-
+    private final JLabel currentPlayerLabel;
+    private final JLabel scoreLabel;
     private MemoryButton selectedButton;
     private int foundPairs;
+    private int currentPlayer;
 
     public MemoryGame() {
         setTitle("Memory Spiel");
@@ -22,6 +24,8 @@ public class MemoryGame extends JFrame {
         buttons = new ArrayList<>();
         coverIcon = new ImageIcon((new ImageIcon("src/images/cover.png")).getImage().getScaledInstance(200, 200, 4));
         imageIcons = loadImages();
+        // currentPlayer auf "1" setzen, da er sonst mit "0" startet und am Anfang sonst 2 Züge hat.
+        currentPlayer = 1;
 
         // Erstelle zwei Kopien von jedem Bild (Kartenpaar)
         List<ImageIcon> pairedIcons = new ArrayList<>();
@@ -39,6 +43,18 @@ public class MemoryGame extends JFrame {
             buttons.add(button);
             panel.add(button);
         }
+
+        // new
+        currentPlayerLabel = new JLabel("Spieler 1 ist an der Reihe");
+        scoreLabel = new JLabel("Punkte: Spieler 1 - 0, Spieler 2 - 0");
+        scoreLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(scoreLabel, BorderLayout.EAST);
+
+        add(currentPlayerLabel, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+        //
 
         add(panel);
         pack();
@@ -72,11 +88,14 @@ public class MemoryGame extends JFrame {
                 selectedButton = null;
 
                 foundPairs++;
-                if (foundPairs == imageIcons.size() / 2) {
+                updateScore();
+                System.out.println(foundPairs + " - " + imageIcons.size());
+                if (foundPairs == imageIcons.size()) {
                     // Alle Paare gefunden
+                    int winner = (getMatchedPairsCount(1) > getMatchedPairsCount(2)) ? 1 : 2;
                     Object[] options = {"Neue Runde", "Beenden"};
                     int choice = JOptionPane.showOptionDialog(this,
-                            "Herzlichen Glückwunsch! Du hast alle Paare gefunden!",
+                            "Herzlichen Glückwunsch, Spieler " + winner + "! Du hast " + getMatchedPairsCount(winner) + " Paare gefunden!",
                             "Spiel beendet",
                             JOptionPane.DEFAULT_OPTION,
                             JOptionPane.INFORMATION_MESSAGE,
@@ -91,11 +110,14 @@ public class MemoryGame extends JFrame {
                     }
                 }
             } else {
+
                 // Kein Paar gefunden
                 Timer timer = new Timer(1000, e -> {
                     selectedButton.hideImage();
                     button.hideImage();
                     selectedButton = null;
+                    // player switcher
+                    switchPlayers();
                 });
                 timer.setRepeats(false);
                 timer.start();
@@ -103,16 +125,31 @@ public class MemoryGame extends JFrame {
         }
     }
 
+    private void switchPlayers() {
+        System.out.println("Aktueller Spieler: " + currentPlayer);
+        currentPlayer = (currentPlayer == 1) ? 2 : 1;
+        currentPlayerLabel.setText("Spieler " + currentPlayer + " ist an der Reihe");
+    }
+
+    private void updateScore() {
+        int player1Score = getMatchedPairsCount(1);
+        int player2Score = getMatchedPairsCount(2);
+        scoreLabel.setText("Punkte: Spieler 1 - " + player1Score + ", Spieler 2 - " + player2Score);
+    }
+
     private void resetGame() {
         selectedButton = null;
+        currentPlayer = 1;
         foundPairs = 0;
+        currentPlayerLabel.setText("Spieler 1 ist an der Reihe");
+        scoreLabel.setText("Punkte: Spieler 1 - 0, Spieler 2 - 0");
 
         // Setze das Spiel zurück, indem alle Karten versteckt und neu gemischt werden
         for (MemoryButton button : buttons) {
             button.hideImage();
             button.setMatched(false);
+            button.setMatchedPlayer(0);
             button.setEnabled(true);
-            button.setBackground(null); // Setze Hintergrundfarbe zurück
         }
         Collections.shuffle(imageIcons);
         int i = 0;
@@ -134,9 +171,52 @@ public class MemoryGame extends JFrame {
         return icons;
     }
 
+    private int getMatchedPairsCount(int player) {
+        int count = 0;
+        for (MemoryButton button : buttons) {
+            if (button.isMatched() && button.getMatchedPlayer() == player) {
+                count++;
+            }
+        }
+        // durch zwei teilen, da ein paar = 2 buttons
+        return count / 2;
+    }
+
+    private void showEndDialog() {
+        int player1Score = getMatchedPairsCount(1);
+        int player2Score = getMatchedPairsCount(2);
+
+        String winner;
+        if (player1Score > player2Score) {
+            winner = "Spieler 1";
+        } else if (player1Score < player2Score) {
+            winner = "Spieler 2";
+        } else {
+            winner = "Unentschieden";
+        }
+
+        Object[] options = {"Neue Runde", "Beenden"};
+        int choice = JOptionPane.showOptionDialog(this,
+                "Herzlichen Glückwunsch! " + winner + " hat das Spiel gewonnen und die meisten Paare gefunden!",
+                "Spiel beendet",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]);
+
+        if (choice == 0) {
+            resetGame();
+        } else {
+            System.exit(0);
+        }
+    }
+
+
     class MemoryButton extends JButton {
         private ImageIcon imageIcon;
         private boolean matched;
+        private int matchedPlayer;
 
         public MemoryButton(ImageIcon icon) {
             setIcon(coverIcon);
@@ -148,6 +228,7 @@ public class MemoryGame extends JFrame {
             setBackground(Color.WHITE);
             imageIcon = icon;
             matched = false;
+            matchedPlayer = 0;
         }
 
         public void showImage() {
@@ -172,6 +253,17 @@ public class MemoryGame extends JFrame {
 
         public void setMatched(boolean matched) {
             this.matched = matched;
+            if (matched) {
+                setMatchedPlayer(currentPlayer);
+            }
+        }
+
+        public int getMatchedPlayer() {
+            return matchedPlayer;
+        }
+
+        public void setMatchedPlayer(int player) {
+            matchedPlayer = player;
         }
     }
 }
